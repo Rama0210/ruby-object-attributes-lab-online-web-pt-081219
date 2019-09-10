ba12d6025363fbb7f80ecff8cefbbc7c8b538ef2
upbeat-torrent-2014 <ramaabrahams@yahoo.com>
2019-09-10 00:21:36 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -13,3 +13,21 @@ end
 def job
   @job
 end
+
+class Person
+
+   def name=(name)
+    @name = name
+  end
+  def name
+    @name
+  end
+
+   def job=(job)
+    @job = job
+  end
+
+   def job
+    @job
+  end
+end
